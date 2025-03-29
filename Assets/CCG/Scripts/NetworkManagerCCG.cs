2df92b0dcdbd875f40a28b54c6b5a2d22f1d2b3c
Enes Sapan <enessapan0@gmail.com>
2025-03-29 19:02:31 +0300

    temp17

--- Assets/CCG/Scripts/NetworkManagerCCG.cs
+++ Assets/CCG/Scripts/NetworkManagerCCG.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using Mirror;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
 
-// Doesnt do anything special but it's set up to be built-upon
 [AddComponentMenu("Network Manager CCG")]
 public class NetworkManagerCCG : NetworkManager
 {
@@ -12,9 +14,30 @@ public class NetworkManagerCCG : NetworkManager
         // Check if offline mode is enabled
         if (PlayerPrefs.GetInt("offlineMode", 0) == 1)
         {
-            // Start as host (server and client)
-            this.StartHost();
+            #if UNITY_ANDROID
+            try 
+            {
+                networkAddress = GetLocalIPAddress();
+                Debug.Log("Using local IP for networkAddress: " + networkAddress);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError("Failed to retrieve local IP address: " + ex.Message);
+            }
+            // Start host after a short delay
+            StartCoroutine(DelayedStartHost());
+            #else
+            // For non-Android builds, just start host immediately
+            StartHost();
+            #endif
         }
+    }
+
+    IEnumerator DelayedStartHost()
+    {
+        // Wait for half a second before starting the host
+        yield return new WaitForSeconds(0.5f);
+        StartHost();
     }
 
     public override void OnStartHost()
@@ -22,7 +45,7 @@ public class NetworkManagerCCG : NetworkManager
         base.OnStartHost();
     }
 
-    // Called when Player connects to the server and joins the game
+    // Called when a player connects to the server and joins the game
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         Transform startPos = GetStartPosition();
@@ -32,9 +55,25 @@ public class NetworkManagerCCG : NetworkManager
         if (PlayerPrefs.GetInt("offlineMode", 0) == 1)
         {
             GameObject aiPlayer = Instantiate(playerPrefab);
-            NetworkServer.Spawn(aiPlayer); 
+            NetworkServer.Spawn(aiPlayer);
             aiPlayer.GetComponent<Player>().InitializeAI();
             aiPlayer.SetActive(true);
         }
     }
+
+    #if UNITY_ANDROID
+    // Retrieves the device's local IP address by creating a UDP socket.
+    public static string GetLocalIPAddress()
+    {
+        string localIP = "";
+        using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+        {
+            // Connect to a known external address to determine the local endpoint.
+            socket.Connect("8.8.8.8", 65530);
+            IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+            localIP = endPoint.Address.ToString();
+        }
+        return localIP;
+    }
+    #endif
 }
